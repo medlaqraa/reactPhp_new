f21247d05bba72a51ac6cb57ad7394a8085cd49f
medlaqraa <139716670+medlaqraa@users.noreply.github.com>
2023-09-22 02:44:31 +0100

    n4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Header from './Components/Header'
 import Home from './Components/Home'
+import Footer from './Components/Footer'
 import UserLists from './Components/UserLists'
 
 function App() {
   return (
     <div>
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <React.Fragment>
+        <h5>Footer</h5>
+    </React.Fragment>
+  )
+}
+export default  Footer
