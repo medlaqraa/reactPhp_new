4d62bd2696f70d789f02765fcfb2601a109138ec
medlaqraa <139716670+medlaqraa@users.noreply.github.com>
2023-09-22 02:10:31 +0100

    new1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+      hello world
+    </>
+  )
 }
-
-export default App;
+export default App
